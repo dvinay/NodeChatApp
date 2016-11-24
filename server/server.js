ff272f13dfae80b05cb93251df91b1e06857e2bd
vinay duvvada <vinay.chowdary518@gmail.com>
2016-11-23 22:38:23 -0800

    adding connect events

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, function() {
+io.on('connection', (socket) => {
+  console.log('new user connected');
+  socket.on('disconnect', () => {
+    console.log('client was disconnected');
+  });
+});
+
+server.listen(port, function() {
   console.log(`app is running on ${port} port`);
 });
